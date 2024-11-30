7a61b39cf48eeed8bcfe15d5528e96fced9154ed
Ana Barbosa <anael.noki@gmail.com>
2024-11-30 20:15:33 -0300

    feat: adiciona verbose_name as models

--- app/models/chat.py
+++ app/models/chat.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=100, unique=True)
-    users_online = models.ManyToManyField(User, related_name="users_online", blank=True)
+    group_name = models.CharField(max_length=100, unique=True, verbose_name="Nome do Grupo")
+    users_online = models.ManyToManyField(User, related_name="users_online", blank=True, verbose_name="Usuários Online")
 
     def __str__(self):
         return self.group_name
@@ -12,14 +12,15 @@ class ChatGroup(models.Model):
 
 class ChatGroupMessage(models.Model):
     group = models.ForeignKey(
-        ChatGroup, related_name="messages", on_delete=models.CASCADE
+        ChatGroup, related_name="messages", on_delete=models.CASCADE, verbose_name="Grupo"
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
-    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    body = models.CharField(max_length=300, verbose_name="Mensagem")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
 
     def __str__(self):
         return f"{self.author.username} : {self.body}"
 
     class Meta:
         ordering = ["-created"]
+
--- app/models/profile.py
+++ app/models/profile.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    displayname = models.CharField(max_length=30, null=True, blank=True)
-    picture = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Usuário")
+    displayname = models.CharField(max_length=30, null=True, blank=True, verbose_name="Nome de Exibição")
+    picture = models.ImageField(upload_to="profile_pics/", null=True, blank=True, verbose_name="Foto de Perfil")
+    bio = models.TextField(null=True, blank=True, verbose_name="Biografia")
 
     def __str__(self):
         return str(self.user)
@@ -23,3 +23,4 @@ class Profile(models.Model):
         if self.picture:
             return self.picture.url
         return f"{settings.STATIC_URL}img/default_pfp.png"
+
